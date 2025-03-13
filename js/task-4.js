5f77a33e19c2daeef2796da40e43f7af7d117f11
zadumka <90329284+zadumka@users.noreply.github.com>
2025-03-13 09:37:30 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -3,8 +3,8 @@ const form = document.querySelector(".login-form");
 const sendForm = (event) => {
   event.preventDefault();
   const form = event.target;
-  const email = form.elements.email.value; // Помилка: відсутній trim()
-  const password = form.elements.password.value; // Помилка: відсутній trim()
+  const email = form.elements.email.value; 
+  const password = form.elements.password.value; 
 
   if (email === "" || password === "") {
     alert("All form fields must be filled in");
